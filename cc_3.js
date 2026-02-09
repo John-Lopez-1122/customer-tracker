d0c1a0baf37d2d164387d0a5e8a6b22f338ad443
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-08 19:56:26 -0500

    Customer updated info

--- cc_3.js
+++ cc_3.js
@@ -11,3 +11,8 @@ console.log(`Purchase: ${customer[2][0]}`)
 
 customer[0].push("Trevor") // Name change
 customer[0].shift()
+
+customer[1].push("TPIndustries.eyefind.info") // email change
+customer[1].shift()
+
+customer[2].push(612.23) // New purchase
