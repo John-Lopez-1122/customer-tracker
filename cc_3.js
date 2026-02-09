e019a87748fbacb90e0ff93045de405b1f6b6537
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-08 19:51:38 -0500

    Error fixes + name change code

--- cc_3.js
+++ cc_3.js
@@ -1,10 +1,13 @@
-let customer = [
+let customer = [            // Array
     ["Joe"],
     ["Joe@joemail.joe"],
     [112.23, 72.65, 165.33]
 
 ]
 
-console.log(`Name: ${customer[0]}`)
+console.log(`Name: ${customer[0]}`) // Console show
 console.log(`Email: ${customer[1]}`)
 console.log(`Purchase: ${customer[2][0]}`)
+
+customer[0].push("Trevor") // Name change
+customer[0].shift()
