d6e81fbe237ec101bd0e9893e457f577221af3cd
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-08 19:45:19 -0500

    first commit

---
+++ cc_3.js
@@ -0,0 +1,10 @@
+let customer = [
+    ["Joe"],
+    ["Joe@joemail.joe"],
+    [112.23, 72.65, 165.33]
+
+]
+
+console.log(`Name: ${customer[0]}`)
+console.log(`Email: ${customer[1]}`)
+console.log(`Purchase: ${customer[2][0]}`)
